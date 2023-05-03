6d0d0b560e1775edaac5a34ae92d4177dd07cf4c
iqbalhosen32 <iqbaldb32@gmail.com>
2023-05-03 18:12:09 +0600

    repository link added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
             <button 
                 type='button'
-                onClick={() => window.open('')}
+                onClick={() => window.open('https://github.com/iqbalhosen32/ai_article_summarizer')}
                 className='black_btn'
             >
                 Github
